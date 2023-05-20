010fbaad1566c8c2c67c598d8eb370cd428cc2a8
MartinEesmae <134137822+MartinEesmae@users.noreply.github.com>
2023-05-20 13:23:53 +0300

    Add files via upload

---
+++ turtle_iseseisevtoo06.py
@@ -0,0 +1,24 @@
+#Martin Eesmäe
+#IKT22
+#20.05.2023
+import turtle
+
+
+
+#Iseseisev töö
+
+for i in range(4):
+    turtle.color("cyan")
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(33)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(33)
+    
+
+
+
+turtle.exitonclick()
+    
